ff791cc6e1fa823facdf8f7997e14b4dd11e5d59
pierrepommier532 <pierrepommier532@gmail.com>
2025-04-08 05:57:32 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,25 @@
+<?php
+// Code snippet for mathematical operations and calculations
+$number1 = 5;
+$number2 = 3;
+
+// Addition
+$result1 = $number1 + $number2;
+echo "The sum of $number1 and $number2 is: $result1";
+
+// Subtraction
+$result2 = $number1 - $number2;
+echo "The difference between $number1 and $number2 is: $result2";
+
+// Multiplication
+$result3 = $number1 * $number2;
+echo "The product of $number1 and $number2 is: $result3";
+
+// Division
+$result4 = $number1 / $number2;
+if ($number2 != 0) {
+    echo "The quotient of $number1 divided by $number2 is: $result4";
+} else {
+    echo "An error occurred: division by zero.";
+}
+?>
