22f58b925d279afbba1b3879910b45f93dfb7317
pierrepommier532 <pierrepommier532@gmail.com>
2025-05-23 05:51:38 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,9 @@
+<?php
+// Define variables from your homework assignment
+$student_name = "John Doe";
+$homework_assignment = "Maths Homework #1";
+
+// Calculate and print solution to Maths Assignment 1
+echo "Solution to Math's Assignment 1: ";
+echo $homework_assignment;
+?>
