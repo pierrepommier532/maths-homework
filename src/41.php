3136ce82c18577688032b8f9b6067ab6583cda1f
pierrepommier532 <pierrepommier532@gmail.com>
2025-05-27 22:22:10 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,26 @@
+<?php
+// Problem 1: Addition of two numbers
+$a = 5;
+$b = 3;
+$c = $a + $b; // c should be 8
+
+// Problem 2: Multiplication of a number by itself
+$x = 4; // x should be 16
+$y = $x * $x; // y should be 16
+
+// Problem 3: Finding the square root of a number (using math functions)
+$number = 16;
+$\sqrt{number} = sqrt($number); // $\sqrt{number}$ should be 4.0
+
+// Problem 4: Calculating the difference between two numbers
+$diff = 8 - 2; // diff should be 6
+
+// Problem 5: Getting a random number (between 1 and 10)
+$random_number = rand(1, 10); // random_number should be between 1 and 10 (inclusive)
+
+// Problem 6: Finding the average of three numbers
+$a = 7; $b = 4; $c = 2;
+$\avg{a,b,c} = ($a + $b + $c) / 3; // $\avg{a,b,c}$ should be 5
+
+// Problem 7: Checking if a number is divisible by another number (using division)
+$divisible_by_10 = 10 % 2; // $divisible_by_10 should be 0
